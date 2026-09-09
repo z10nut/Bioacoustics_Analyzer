c8ee022c40eba1414d0ff0132d244bba97b07227
Ionut <moraruionut899@gmail.com>
2026-09-09 14:34:41 +0300

    Test

--- build_app.py
+++ build_app.py
@@ -1,9 +1,9 @@
 import os
-import shutil
 import sklearn
 import PyInstaller.__main__
 
 sep = ';' if os.name == 'nt' else ':'
+
 args = [
     'main.py',
     '--name=BioacousticsAnalyzer',
@@ -15,20 +15,16 @@ args = [
     '--collect-all=sklearn',
     '--collect-all=librosa',
     '--collect-all=scipy',
+    '--collect-all=tensorflow', 
     f'--add-data=gui/styles/main_styles.qss{sep}gui/styles',
     '--noconfirm'
 ]
 
-PyInstaller.__main__.run(args)
-
 if os.name == 'nt':
     sklearn_dir = os.path.dirname(sklearn.__file__)
-    sklearn_libs_src = os.path.join(sklearn_dir, '.libs')
+    sklearn_libs = os.path.join(sklearn_dir, '.libs')
     
-    dest_dir = os.path.join('dist', 'BioacousticsAnalyzer', '_internal', 'sklearn', '.libs')
-    
-    if os.path.exists(sklearn_libs_src):
-        if os.path.exists(dest_dir):
-            shutil.rmtree(dest_dir)
-            
-        shutil.copytree(sklearn_libs_src, dest_dir)
+    if os.path.exists(sklearn_libs):
+        args.append(f'--add-data={sklearn_libs}{sep}sklearn/.libs')
+
+PyInstaller.__main__.run(args)
