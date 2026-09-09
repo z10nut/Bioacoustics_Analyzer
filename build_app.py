4527e920872eae1329e9e83f7d0ae1fbcd05938c
Ionut <moraruionut899@gmail.com>
2026-09-09 14:52:02 +0300

    Test

--- build_app.py
+++ build_app.py
@@ -1,10 +1,13 @@
 import os
+import shutil
 import sklearn
-import PyInstaller.__main__
+import subprocess
+import sys
 
 sep = ';' if os.name == 'nt' else ':'
 
 args = [
+    sys.executable, '-m', 'PyInstaller',
     'main.py',
     '--name=BioacousticsAnalyzer',
     '--windowed',
@@ -15,16 +18,26 @@ args = [
     '--collect-all=sklearn',
     '--collect-all=librosa',
     '--collect-all=scipy',
-    '--collect-all=tensorflow', 
+    '--collect-all=tensorflow',
     f'--add-data=gui/styles/main_styles.qss{sep}gui/styles',
     '--noconfirm'
 ]
 
+print("Running PyInstaller...")
+subprocess.run(args, check=True)
+
+print("PyInstaller finished. Applying manual patch for scikit-learn DLLs...")
+
 if os.name == 'nt':
     sklearn_dir = os.path.dirname(sklearn.__file__)
-    sklearn_libs = os.path.join(sklearn_dir, '.libs')
+    sklearn_libs_src = os.path.join(sklearn_dir, '.libs')
+    dest_dir = os.path.join('dist', 'BioacousticsAnalyzer', '_internal', 'sklearn', '.libs')
     
-    if os.path.exists(sklearn_libs):
-        args.append(f'--add-data={sklearn_libs}{sep}sklearn/.libs')
-
-PyInstaller.__main__.run(args)
+    if os.path.exists(sklearn_libs_src):
+        if os.path.exists(dest_dir):
+            shutil.rmtree(dest_dir)
+            
+        shutil.copytree(sklearn_libs_src, dest_dir)
+        print(f"SUCCESS: Copied content from {sklearn_libs_src} to {dest_dir}")
+    else:
+        print(f"WARNING: Source folder {sklearn_libs_src} not found on this system!")
