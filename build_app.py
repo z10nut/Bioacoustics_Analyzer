192c1e933426a41f2f79fbbc3330a63f844ac704
Ionut <moraruionut899@gmail.com>
2026-09-09 15:24:13 +0300

    test

--- build_app.py
+++ build_app.py
@@ -1,12 +1,9 @@
 import os
-import shutil
-import subprocess
-import sys
+import PyInstaller.__main__
 
 sep = ';' if os.name == 'nt' else ':'
 
 args = [
-    sys.executable, '-m', 'PyInstaller',
     'main.py',
     '--name=BioacousticsAnalyzer',
     '--windowed',
@@ -22,24 +19,4 @@ args = [
     '--noconfirm'
 ]
 
-print("Running PyInstaller...")
-subprocess.run(args, check=True)
-
-print("PyInstaller finished. Applying patch for scikit-learn DLLs...")
-
-if os.name == 'nt':
-    internal_dir = os.path.join('dist', 'BioacousticsAnalyzer', '_internal')
-    sklearn_libs_dest = os.path.join(internal_dir, 'sklearn', '.libs')
-    
-    os.makedirs(sklearn_libs_dest, exist_ok=True)
-    
-    msvcp_src = os.path.join(internal_dir, 'msvcp140.dll')
-    msvcp140_1_src = os.path.join(internal_dir, 'msvcp140_1.dll')
-    
-    if os.path.exists(msvcp_src):
-        shutil.copy(msvcp_src, sklearn_libs_dest)
-        print("SUCCESS: Copied msvcp140.dll to sklearn/.libs")
-        
-    if os.path.exists(msvcp140_1_src):
-        shutil.copy(msvcp140_1_src, sklearn_libs_dest)
-        print("SUCCESS: Copied msvcp140_1.dll to sklearn/.libs")
+PyInstaller.__main__.run(args)
